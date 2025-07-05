3977e4eb64f07189963a0b80bf108b7bf308d521
Alexander Kostarev <UniverAlex@ya.ru>
2025-07-05 19:33:50 +0300

    Компонента для отображения поля заказа

---
+++ src/components/OrderProp.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { NText } from 'naive-ui';
+defineProps<{ label: string; value: string }>();
+</script>
+<template>
+  <div class="order-prop">
+    <span class="order-prop__label">
+      <n-text class="label__text">
+        {{ label }}
+      </n-text>
+    </span>
+    <n-text class="order-prop__value">
+      {{ value }}
+    </n-text>
+  </div>
+</template>
+
+<style scoped>
+.order-prop {
+  display: inline-flex;
+  width: 100%;
+}
+
+.order-prop__label {
+  display: flex;
+  position: relative;
+  width: 40%;
+}
+
+.order-prop__label::after {
+  content: '';
+  height: 100%;
+  width: 100%;
+  border-top: 1.5px dotted #d1d1d6;
+  position: absolute;
+  z-index: -1;
+  top: 12px;
+}
+
+.order-prop__value {
+  width: 50%;
+}
+
+.label__text {
+  color: #868695;
+  background-color: #fff;
+  border-right: 6px solid #fff;
+}
+</style>
